6c27fe7c01484d55b174d3ef9aaea7239903633e
alexnakov <alex.nakov3@gmail.com>
2024-06-17 14:28:14 +0100

    Creating same starting grid in c2

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ window.onload = function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
 
+    console.log(canvas.width)
+    console.log(canvas.height)
+
     // Grid spacing cannot be relative, it needs to be absolute
     const gridSpacingX = 50;
     const gridSpacingY = 25;
 
     ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, 350, 250)
+    ctx.fillRect(0, 0, 300, 150)
     ctx.strokeStyle = 'whitesmoke';
     ctx.lineWidth = 1;
 
@@ -37,4 +40,46 @@ window.onload = function() {
     ctx.moveTo(150, 0)
     ctx.lineTo(150, 150)
     ctx.stroke()
+
+    // fill origin grey
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(148, 73, 4, 4)
+}
+
+// Ideally you would have the matrix params to pass
+const showCanvas2 = function() {
+    const canvas = document.getElementById('canvas2');
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, 350, 250)
+    ctx.strokeStyle = 'whitesmoke';
+    ctx.lineWidth = 1;
+
+    ctx.fillStyle = 'red';
+
+    for (let y = 0; y <= canvas.height; y += 25) {
+        for (let x = 0; x <= canvas.width; x += 1) {
+            var x_ = x
+            var y_ = y
+            var xCoord = x_;
+            var yCoord = y_;
+            
+            ctx.fillRect(xCoord, yCoord, 1, 1)
+        }
+    }
+
+    for (let x = 0; x <= canvas.width; x += 50) {
+        for (let y = 0; y <= canvas.height; y += 1) {
+            var x_ = x
+            var y_ = y
+            var xCoord = x_;
+            var yCoord = y_;
+            
+            ctx.fillRect(xCoord, yCoord, 1, 1)
+        }
+    }
+
+    ctx.fillStyle = 'aqua';
+    ctx.fillRect(148, 73, 4, 4)
 }
