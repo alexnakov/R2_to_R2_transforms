6e4386ebbd4e5ac8cd42d3bf6b1078b3d7e9fba7
alexnakov <alex.nakov3@gmail.com>
2024-06-24 12:15:18 +0100

    Extended lines longer than svg canvas

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const sqrt2 = Math.sqrt(2)
+const SQRT2 = Math.sqrt(2)
 
 const addLineToSvg = function (svgParent, x1, y1, x2, y2) {
   var newLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
@@ -13,20 +13,18 @@ const addLineToSvg = function (svgParent, x1, y1, x2, y2) {
 const svgParent = document.getElementById("svg1")
 
 for (let i = 0; i <= 300; i+=50) {
-  addLineToSvg(svgParent, i, 0, i, 300)
+  addLineToSvg(svgParent, i, -1000, i, 1300)
 }
 
 for (let i = 0; i <= 300; i+=50) {
-  addLineToSvg(svgParent, 0, i, 300, i)
+  addLineToSvg(svgParent, -1000, i, 1300, i)
 }
 
-const rotLine45 = function() {
-  const line1 = document.getElementById('line1')
-
-  let x1 = parseFloat(line1.getAttribute('x1'));
-  let y1 = parseFloat(line1.getAttribute('y1'));
-  let x2 = parseFloat(line1.getAttribute('x2'));
-  let y2 = parseFloat(line1.getAttribute('y2'));
+const rotLine45 = function(svgLine) {
+  let x1 = parseFloat(svgLine.getAttribute('x1'));
+  let y1 = parseFloat(svgLine.getAttribute('y1'));
+  let x2 = parseFloat(svgLine.getAttribute('x2'));
+  let y2 = parseFloat(svgLine.getAttribute('y2'));
 
   const center_x = 150
   const center_y = 150
@@ -38,11 +36,11 @@ const rotLine45 = function() {
   y2 = y2 - center_y
 
   // Rotation around O
-  r_x1 = sqrt2/2*(x1-y1)
+  r_x1 = SQRT2/2*(x1-y1)
   // You just changed the x-val so next line uses the new one 😅
-  r_y1 = sqrt2/2*(x1+y1) 
-  r_x2 = sqrt2/2*(x2-y2)
-  r_y2 = sqrt2/2*(x2+y2)
+  r_y1 = SQRT2/2*(x1+y1) 
+  r_x2 = SQRT2/2*(x2-y2)
+  r_y2 = SQRT2/2*(x2+y2)
 
   // Backtranslation
   x1 = r_x1 + center_x
@@ -50,10 +48,13 @@ const rotLine45 = function() {
   y1 = r_y1 + center_y
   y2 = r_y2 + center_y
 
-  line1.setAttribute('x1', x1)
-  line1.setAttribute('y1', y1)
-  line1.setAttribute('x2', x2)
-  line1.setAttribute('y2', y2)
+  svgLine.setAttribute('x1', x1)
+  svgLine.setAttribute('y1', y1)
+  svgLine.setAttribute('x2', x2)
+  svgLine.setAttribute('y2', y2)
 }
 
-rotLine45()
+let lines = document.getElementsByTagNameNS('http://www.w3.org/2000/svg', 'line')
+for (let i = 0; i < lines.length; i++) {
+  rotLine45(lines[i])
+}
